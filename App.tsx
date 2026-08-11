36713544f468c052c938142e4c8ea983089278e4
uloba-rgb <uloba@brandxnetworking.com>
2026-08-10 23:55:43 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
+import ErrorBoundary from "./ErrorBoundary";
 import LiveGraph from "./components/LiveGraph";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
