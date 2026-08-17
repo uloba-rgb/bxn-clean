d60a3ab7704e22685936cbd419f790008c9c3dc4
uloba-rgb <uloba@brandxnetworking.com>
2026-08-17 03:44:12 -0700

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -211,7 +211,7 @@ export default function Home() {
             </div>
           </div> 
     {/* RIGHT SIDE — BXN VIDEO */}
-<div className="hidden lg:flex items-center justify-center -mt-40">
+<div className="hidden lg:flex items-center justify-center -mt-60">
   <video
     autoPlay
     loop
