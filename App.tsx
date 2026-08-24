cc7986d40f7ea7bd8f0f1a3d4f0cb351bd9fe3e2
uloba-rgb <uloba@brandxnetworking.com>
2026-08-24 03:47:31 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ import Home from "./Home";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
+import Privacy from "./Privacy";
+import Terms from "./Terms";
 
 
 /**
@@ -43,7 +45,8 @@ function Router() {
     <Route path="/about" component={About} />
     <Route path="/services" component={Services} />
     <Route path="/contact" component={Contact} /> 
-    <Route path="/privacy" component={Privacy} />
+      <Route path="/privacy" component={Privacy} />
+  <Route path="/terms" component={Terms} />
 
     <Route path="/404" component={NotFound} />
     <Route component={NotFound} />
